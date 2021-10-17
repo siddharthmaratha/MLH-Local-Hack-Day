870068f5598c01a9fd5d758bc5e2789665311f4f
Siddharth Maratha <80692281+siddharthmaratha@users.noreply.github.com>
2021-10-17 20:20:43 +0530

    Add files via upload

---
+++ Day 7/playlist-project-master/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, session
+from flask.ext.session import Session
+from spotifyThing import authorize
+import sys
+import spotipy
+import spotipy.util as util
+from showPlaylists import show_tracks
+from flask import redirect
+from flask import render_template
+from flask import request
+from spotifychooser import *
+import copy
+from spotipy import Spotify
+app = Flask(__name__)
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
+app.debug = True
+
+
+from flask import render_template
+
+@app.route('/')
+def hello(name=None, code=None):
+    code = request.args.get("code", None)
+    if code is None:
+        return render_template('index.html', name=name)
+    else:
+        print "we have a code, which is: " + code
+        sp_oauth = authorize()
+        token = sp_oauth.get_access_token(code)
+        sp = spotipy.Spotify(auth=token['access_token'])
+        user = sp.me()
+        session['uid'] = user['id']
+        session['token'] = token['access_token']
+        return render_template('index2.html', code=code)
+
+@app.route('/auth/')
+def test(name=None):
+    return redirect(authorize().get_authorize_url(), code=302)
+
+@app.route('/tracks/')
+def tracks(name=None):
+    name = show_tracks(session.get('token', None))
+    return render_template('index2.html', name=name)
+
+@app.route('/chooser/<playlistid>')
+def chooser(playlistid=None, userid=None):
+    userid = session.get('uid', None)
+    token = session.get('token', None)
+    sp = spotipy.Spotify(auth=token)
+    get_playlist_json(sp, userid, playlistid)
+    return render_template('index2.html', userid=userid, playlistid=playlistid)
+
+        
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
---
+++ Day 7/showPlaylists.py
@@ -0,0 +1,17 @@
+# shows a user's playlists (need to be authenticated via oauth)
+
+import sys
+import spotipy
+import spotipy.util as util
+from spotifyThing import authorize
+
+def show_tracks(token):
+    
+    sp = spotipy.Spotify(auth=token)
+    username = sp.me()['id']
+    playlists = sp.user_playlists(username)
+    returnList = []
+    for playlist in playlists['items']:
+        if playlist['owner']['id'] == username:
+            returnList.append({'title': playlist['name'], 'id': playlist['id']})
+    return returnList
